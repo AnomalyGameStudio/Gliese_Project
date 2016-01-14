725c201ebafc4c5b9357b936871ac3871228b37f
elopes42 <elopes06@gmail.com>
2016-01-14 18:11:23 -0200

    Acertando GC

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,13 +5,13 @@ public class GameController : MonoBehaviour
 {
 	// Singleton from GameController class
 	public static GameController instance;
-
+	
 	// Stores the global gravity. This is set by the Player Controller
-	public float gravity = -180;
-
+	//public float gravity = -180;
+	
 	// Stores the current checkpoint the player is
 	Vector3 checkpoint = Vector3.zero;
-
+	
 	void Awake()
 	{
 		// If there isn't a stance of GameController yet
@@ -21,20 +21,20 @@ public class GameController : MonoBehaviour
 			instance = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController> ();
 		}
 	}
-
+	
 	// Sets the Last checkpoint the player has passed
 	public void SetCheckpoint(Vector3 checkpoint)
 	{
 		this.checkpoint = checkpoint;
 	}
-
+	
 	public void SpawnPlayer()
 	{
-
+		
 	}
-
+	
 	public void KillPlayer()
 	{
-
+		
 	}
 }
