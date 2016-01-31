d3050a745d543c85557296e69acf2236e616a1c4
elopes42 <elopes06@gmail.com>
2016-01-31 01:56:29 -0200

    Botão e porta feitos

---
+++ Assets/Scripts/ButtonHandler.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonHandler : MonoBehaviour 
+{
+	public DoorController door;
+
+	/*
+	void OnTriggerStay(Collider collider)
+	{
+		// Gets the Action Button
+		bool actionButton = Input.GetButtonDown("Action");
+		
+		// If the player is in the collider and the action button is pressed
+		if(collider.tag == "Player" && actionButton)
+		{
+			// Signals the platform to move to the next platform
+			door.ManualMove();
+		}
+	}
+*/
+
+	void OnTriggerEnter(Collider collider)
+	{
+		if(collider.tag == "Player" || collider.tag == "Draggable")
+		{
+			door.ManualMove(false);
+		}
+	}
+	
+	void OnTriggerExit()
+	{
+			door.ManualMove(true);
+	}
+
+}
