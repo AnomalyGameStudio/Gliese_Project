64ea3c6e5285f03b58e1a56c0edfeaab923d1b9b
Felipe Guimarães Cervera <guimaraescerverafelipe@gmail.com>
2016-02-04 19:31:58 -0200

    New Scene + Other shits
    Na pasta teste está o orc e as armas. o Script está na pasta script

---
+++ Assets/Scripts/IdleChange.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IdleChange : MonoBehaviour {
+
+	// Use this for initialization
+	Animator animator;
+	
+	void Start()
+	{
+		animator = GetComponent<Animator>();
+	}
+	
+	void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.Alpha1))
+		{
+			animator.SetFloat("Idle", 1f);
+		}
+		
+		if(Input.GetKeyDown(KeyCode.Alpha2))
+		{
+			animator.SetFloat("Idle", 2f);
+		}
+	}
+}
